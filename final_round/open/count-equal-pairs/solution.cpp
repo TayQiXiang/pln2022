ce3e634aaf7afd1af440e1acb0fdabcb3844b216
Tay Qi Xiang <tayqixiang@gmail.com>
2022-04-03 20:59:37 +0800

    Update solution.cpp

--- final_round/open/count-equal-pairs/solution.cpp
+++ final_round/open/count-equal-pairs/solution.cpp
@@ -12,13 +12,8 @@ int main() {
         int n, m;
         cin >> n >> m;
 
-        for(int i = 0; i < n; i++) {
-            cin >> a[i];
-        }
-
-        for(int i = 0; i < m; i++) {
-            cin >> b[i];
-        }
+        for(int i = 0; i < n; i++) cin >> a[i];
+        for(int i = 0; i < m; i++) cin >> b[i];
 
         long long ans = 0;
         int aIdx = 0, aCount = 1;
