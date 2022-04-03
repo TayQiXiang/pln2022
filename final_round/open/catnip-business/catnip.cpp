4d586e5e9563b4e9e9897276fe980824a63afe96
Tay Qi Xiang <tayqixiang@gmail.com>
2022-04-03 19:37:13 +0800

    Update catnip.cpp

--- final_round/open/catnip-business/catnip.cpp
+++ final_round/open/catnip-business/catnip.cpp
@@ -1,34 +1,22 @@
-#include <bits/stdc++.h>
+#include <iostream>
+ 
 using namespace std;
-#define forn(i, n) for (int i = 0; i < int(n); i++)
-typedef long long ll;
-
-void solve() {
-    int n;
-    cin >> n;
-    long long sum = 0, ans = 0;
-    for (int i = 0; i < n; i++) {
-        int x;
-        cin >> x;
-        sum += x;
-        if (sum < 0)
-            ans -= sum;
-        else
-            ans += sum;
-    }
-    cout << ans << endl;
-}
-
+ 
 int main() {
-#ifndef ONLINE_JUDGE
-    freopen("../input.txt", "r", stdin);
-    freopen("../output.txt", "w", stdout);
-#endif
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int t = 0;
+    int t, n, x;
     cin >> t;
-    while (t--) solve();
+ 
+    while(t--) {
+        cin >> n;
+        int ans = 0, owe = 0;
+        while(n--) {
+            cin >> x;
+            owe += x;
+            ans += abs(owe);
+        }
+ 
+        cout << ans << "\n";
+    }
+ 
+    return 0;
 }
