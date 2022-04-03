7a5e556cfe35d8dd1eba6f5479b2456a9ed2f687
Tay Qi Xiang <tayqixiang@gmail.com>
2022-04-03 19:56:07 +0800

    Update and rename password.cpp to solution.cpp

--- final_round/open/security-system/solution.cpp
+++ final_round/open/security-system/solution.cpp
@@ -6,10 +6,13 @@ long long mod = 1000000007;
 
 void pascal(int n) {
     for (int i = 0; i <= n; ++i) P[i][0] = P[i][i] = 1;
-    for (int i = 2; i <= n; ++i)
-        for (int j = 1; j <= i; ++j)
+    for (int i = 2; i <= n; ++i) {
+        for (int j = 1; j <= i; ++j) {
             P[i][j] = (P[i - 1][j] + P[i - 1][j - 1]) % mod;
+        }
+    }
 }
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -18,10 +21,13 @@ int main() {
     cin >> n >> a >> b;
     pascal(n + max(a, b));
 
-    for (int i = 0; i <= a; ++i)
-        for (int j = 0; j <= b; ++j)
+    for (int i = 0; i <= a; ++i) {
+        for (int j = 0; j <= b; ++j) {
             ans += (P[n + i - 1][n - 1] * P[n + j - 1][n - 1]) % mod;
-            ans%= mod;
-    cout << ans % mod<<endl;
+            ans %= mod;
+        }
+    }
+
+    cout << ans % mod << endl;
     return 0;
 }
